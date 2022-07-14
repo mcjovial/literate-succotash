4c7a217b058706d9fcf25e17fbe7ba52473a7188
mcjovial <emmanuelelias455@gmail.com>
2022-07-14 15:45:44 +0100

    layout components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,15 @@
+import { Navbar, Services, Transactions, Welcome } from "./components"
+
 const App = () => {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div className="min-h-screen">
+      <div className="gradient-bg-welcome">
+        <Navbar />
+        <Welcome />
+      </div>
+      <Services />
+      <Transactions />
+    </div>
   )
 }
 
