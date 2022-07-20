48a43ceb1ff45a3490651c99d97df2ca237ccc56
mcjovial <emmanuelelias455@gmail.com>
2022-07-20 16:40:01 +0100

    client done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,15 @@
-import { Navbar, Services, Transactions, Welcome } from "./components"
+import { Navbar, Welcome, Footer, Services, Transactions } from "./components";
 
-const App = () => {
-  return (
-    <div className="min-h-screen">
-      <div className="gradient-bg-welcome">
-        <Navbar />
-        <Welcome />
-      </div>
-      <Services />
-      <Transactions />
+const App = () => (
+  <div className="min-h-screen">
+    <div className="gradient-bg-welcome">
+      <Navbar />
+      <Welcome />
     </div>
-  )
-}
+    <Services />
+    <Transactions />
+    <Footer />
+  </div>
+);
 
-export default App
+export default App;
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,15 +1,15 @@
-import React, { useState } from "react";
+import React from "react";
 import { HiMenuAlt4 } from "react-icons/hi";
 import { AiOutlineClose } from "react-icons/ai";
 
-import logo from "../assets/images/logo.png";
+import logo from "../../images/logo.png";
 
 const NavBarItem = ({ title, classprops }) => (
   <li className={`mx-4 cursor-pointer ${classprops}`}>{title}</li>
 );
 
 const Navbar = () => {
-  const [toggleMenu, setToggleMenu] = useState(false);
+  const [toggleMenu, setToggleMenu] = React.useState(false);
 
   return (
     <nav className="w-full flex md:justify-center justify-between items-center p-4">
